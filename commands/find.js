0cdd90fea0bd161bce04bb5a20c1f4059c109534
Arcia125 <tauren125@gmail.com>
2017-02-20 20:20:57 -0600

    add async file reading

--- commands/find.js
+++ commands/find.js
@@ -1,6 +1,8 @@
 const baseChalk = require(`chalk`);
 const ProgessBar = require(`progress`);
 const fs = require(`fs`);
+const glob = require(`glob`);
+const async = require(`async`);
 
 const { getFileText, getLines, findText } = require(`../utils/fileText`);
 
@@ -16,49 +18,52 @@ module.exports = (program) => {
         .option(`-I, --if`, `prints whether <text> was found in [file]`)
         .option(`-N, --numbers`, `prints line number of each line containing <text> (default output)`)
         .option(`-n, --number-array`, `prints the line numbers as an array`)
-        .option(`-f, --file-name`, `prints the name of [file]`)
+        .option(`-F, --file-name`, `prints the name of [file]`)
+        .option(`-f, --no-filename`, `removes file names from output`)
         .option(`-v, --invert-match`, `inverts search selection`)
         .option(`-c, --no-colors`, `removes colors from output`)
         .option(`-o, --only-matching`, `returns only the part of each line matching <text>`)
-        .action((text, file = `index.js`, options) => {
-            // progess bar options
-            const fileSize = fs.statSync(file).size;
-            const progressOpts = {
-                width: 20,
-                total: fileSize,
-                clear: true,
-            };
-            // progress bar object creation
-            const bar = new ProgessBar(`reading file [:bar] :percent :etas`, progressOpts);
+        .action((text, fileName = `index.js`, options) => {
+            const fileList = glob(fileName, { nodir: true }, (err, files) => {
+                if (err) {
+                    console.log(`Error occured while creating glob pattern: ${err}`);
+                }
+                async.forEachLimit(files, 2000, (file) => {
+                    // toggles colors with the flag -c, --only-matching
+                    const chalk = new baseChalk.constructor({ enabled: options.noColors || options.colors });
+                    getFileText(file)
+                        .then((textData) => {
+                            const lines = getLines(textData);
+                            const foundTextResult = lines.reduce((a, b) => a.concat(findText(b, text, { invert: options.invertMatch, onlyMatching: options.onlyMatching })), []);
+                            const numberedLines = foundTextResult.map((line, lineNumber) => ({ lineNumber, line }));
+                            const foundText = numberedLines.filter(lineObj => lineObj.line !== null);
+                            const noOutputOpts = !(options.if || options.numberArray || options.numbers || options.lines);
+                            const textWasFound = foundText.find(line => line.line.length > 0);
 
-            // toggles colors with the flag -c, --only-matching
-            const chalk = new baseChalk.constructor({ enabled: options.noColors || options.colors });
+                            if (!textWasFound && !options.if) {
+                                return false;
+                            }
 
-            // callback that will be called by getFileText on every chunk when
-            // reading a file
-            const onData = (chunk) => {
-                bar.tick(chunk.length);
-            };
+                            (options.filename && (textWasFound || options.if)) && log(`\n${chalk.bgMagenta(file)}`);
 
-            getFileText(file, onData)
-                .then((textData) => {
-                    const lines = getLines(textData);
-                    const foundTextResult = lines.reduce((a, b) => a.concat(findText(b, text, { invert: options.invertMatch, onlyMatching: options.onlyMatching })), []);
-                    const numberedLines = foundTextResult.map((line, lineNumber) => ({ lineNumber, line }));
-                    const foundText = numberedLines.filter(lineObj => lineObj.line !== null);
-                    const noOutputOpts = !(options.if || options.numberArray || options.numbers || options.lines);
+                            !options.filename && log(`\n`);
 
-                    options.fileName && log(chalk.black.bgWhite(file));
-                    options.if && log(foundText.length > 0 ? chalk.bold.green(true) : chalk.bold.red(false));
-                    options.numberArray && log(foundText.map(line => line.lineNumber));
-                    ((options.numbers || options.lines) || noOutputOpts) && foundText.forEach((line) => {
-                        const spaceCount = 5 - line.lineNumber.toString().length;
-                        const lineNumberText = options.numbers || noOutputOpts ? `${line.lineNumber}${' '.repeat(spaceCount > 0 ? spaceCount : 1)}` : ``;
-                        const lineText = options.lines || noOutputOpts ? `${line.line}` : ``;
-                        log(`${lineNumberText}${lineText}`);
-                    });
-                })
-                .catch(err => log(`There was an error getting the file text: ${err}`));
+                            options.if && log(foundText.length > 0 ? chalk.bold.green(true) : chalk.bold.red(false));
+
+                            options.numberArray && textWasFound && log(foundText.map(line => line.lineNumber));
+
+
+                            ((options.numbers || options.lines) || noOutputOpts)
+                            && foundText.forEach((line) => {
+                                const spaceCount = 5 - line.lineNumber.toString().length;
+                                const lineNumberText = options.numbers || noOutputOpts ? `${line.lineNumber}${' '.repeat(spaceCount > 0 ? spaceCount : 1)}` : ``;
+                                const lineText = options.lines || noOutputOpts ? `${line.line}` : ``;
+                                log(`${lineNumberText}${chalk.yellow(lineText)}`);
+                            });
+                        })
+                        .catch(error => log(`There was an error getting the file text: ${error}`));
+                });
+            });
         });
     return program;
 };
--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 #!/usr/bin/env node
 
 const program = require(`commander`);
-const chalk = require(`chalk`);
-const ProgessBar = require(`progress`);
 const pkgJson = require(`./package.json`);
 const execCmd = require(`child_process`).exec;
 
@@ -16,7 +14,12 @@ require(`./commands/find`)(program);
 program
     .command(`help [cmd]`)
     .description(`provides help for [cmd]`)
-    .action(cmd => cmd ? execCmd(`nwf ${cmd} -h`, (err, stdout, stderr) => console.log(stdout)) : program.help());
+    .action((cmd) => cmd ? execCmd(`nwf ${cmd} -h`, (err, stdout, stderr) => {
+        if (err) {
+            throw err;
+        }
+        return console.log(stdout);
+    }) : program.help());
 
 program.parse(process.argv);
 
